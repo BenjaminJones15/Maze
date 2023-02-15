585b5852e8084bfa17e5dfee6c59f214c63a4a97
LegitLizard <36738275+LegitLizard@users.noreply.github.com>
2023-02-15 12:44:42 -0700

    Teleporting into maze works
    Leaving lights for now, come back to later.

---
+++ Assets/Hard.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class Hard : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform Target1;
+    public Transform Target2;
+    public Transform Target3;
+    public Transform Target4;
+    public Transform Target5;
+    public Light Sun;
+    Random rnd = new Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("body"))
+        {
+            Debug.Log("hard");
+            int randint = rnd.Next(1, 6);        //teleports player to random area
+            if (randint == 1)
+            {
+                Player.transform.position = Target1.transform.position;
+            }
+            else if (randint == 2)
+            {
+                Player.transform.position = Target2.transform.position;
+            }
+            else if (randint == 3)
+            {
+                Player.transform.position = Target3.transform.position;
+            }
+            else if (randint == 4)
+            {
+                Player.transform.position = Target4.transform.position;
+            }
+            else if (randint == 5)
+            {
+                Player.transform.position = Target5.transform.position;
+            }
+            Sun.enabled = !Sun.enabled;     //turns off lights, needs to work better.
+            RenderSettings.ambientLight = Color.black;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
