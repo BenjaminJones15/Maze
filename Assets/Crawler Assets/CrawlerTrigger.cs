757894753c2e0cd7e7c953093a68131cb127338b
Benjamin Jones <benjaminjones15@gmail.com>
2023-03-25 16:45:13 -0600

    Crawler Animations In Progress

--- Assets/Crawler Assets/CrawlerTrigger.cs
+++ Assets/Crawler Assets/CrawlerTrigger.cs
@@ -14,7 +14,8 @@ public class CrawlerTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CrawlerMove.SetTrigger("Move");
+        CrawlerMove.SetTrigger("Chase");
     }
 
     void OnTriggerEnter(Collider collision)
