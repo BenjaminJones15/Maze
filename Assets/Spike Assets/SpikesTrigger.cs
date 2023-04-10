b00531b76b97d76d921a257a8eaca162a4d40e4f
Benjamin <benjaminjones15@gmail.com>
2023-04-10 14:08:39 -0600

    Spike 1 all working!

--- Assets/Spike Assets/SpikesTrigger.cs
+++ Assets/Spike Assets/SpikesTrigger.cs
@@ -7,21 +7,14 @@ public class SpikesTrigger : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip clip;
     public float volume=1f;
-    public Animator SpikesMove11;
-    public Animator SpikesMove12;
-    public Animator SpikesMove13;
-    public Animator SpikesMove14;
-    public Animator SpikesMove2;
-    public Animator BirdMove = null;
-    public GameObject Spikes11;
-    public GameObject Spikes12;
-    public GameObject Spikes13;
-    public GameObject Spikes14;
-    public GameObject Spikes2;
+    public Animator SpikesMove;
+    public GameObject Spikes;
+    public Animator BirdMove;
     public GameObject Player;
     public GameObject Wall1;
     public GameObject Wall2 = null;
     public GameObject Wall3 = null;
+    public Component[] list;
 
     // Start is called before the first frame update
     void Start()
@@ -34,13 +27,13 @@ public class SpikesTrigger : MonoBehaviour
         if (collision.CompareTag("body"))
         {
             audioSource.PlayOneShot(clip, volume);
-            Spikes11.GetComponent<MeshRenderer>().enabled = true;
-            Spikes2.GetComponent<MeshRenderer>().enabled = true;
-            SpikesMove11.SetTrigger("Descend");
-            SpikesMove12.SetTrigger("Descend");
-            SpikesMove13.SetTrigger("Descend");
-            SpikesMove14.SetTrigger("Descend");
-            SpikesMove2.SetTrigger("Descend");
+            SpikesMove.SetTrigger("Descend");
+            list = Spikes.GetComponentsInChildren<MeshRenderer>();
+            {
+                foreach (MeshRenderer spike in list)
+                    spike.enabled = true;
+            }
+
             Wall1.GetComponent<MeshRenderer>().enabled = true;
             if(Wall2 != null) {
                 Wall2.GetComponent<MeshRenderer>().enabled = true;
